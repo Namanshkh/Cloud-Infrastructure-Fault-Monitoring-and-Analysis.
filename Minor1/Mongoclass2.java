14a2c692188ad5a921cc37d6ad06083d7e29a558
Namansh Khandelwal <83030396+Namanshkh@users.noreply.github.com>
2021-12-09 11:12:32 +0530

    Mongo class

---
+++ Minor1/Mongoclass2.java
@@ -0,0 +1,34 @@
+import com.mongodb.MongoClient;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.DB;
+
+
+public class MongoClass2 {
+
+	public static void main(String[] args) throws UnknownHostException {
+		
+		MongoClient mongoClient=new MongoClient("localhost",27017);
+		DB db=mongoClient.getDB("admin");
+		DBCollection coll=db.getCollection("mycollection");
+		
+		DBCursor cursor=coll.find();
+		
+		while(cursor.hasNext())
+		{
+			int i=1;
+			System.out.println(cursor.next());
+			i++;
+		}
+			
+		System.out.println("Connection Established Successfully");
+		
+
+	}
+
+}
